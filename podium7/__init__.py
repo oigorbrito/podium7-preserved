8dbccb1ec495526100f9b3c1a1efda215ceeb465
tihotm <igorbrito.n1@hotmail.com>
2026-08-19 18:45:37 -0300

    Expose autonomy primitives

--- podium7/__init__.py
+++ podium7/__init__.py
@@ -1,17 +1,7 @@
 from .ai_discovery import ValidatedExtractionArtifact, validate_extraction_artifact
+from .autonomy import AcquisitionCache, Checkpoint, EnrichmentJob, JobResult, JobState, RateLimiter, RetryPolicy, identify_gaps, plan_jobs, run_job
 from .document_extraction import DocumentExtractionReport, extract_ford_dark_horse_document
-from .domain import (
-    AutomotiveIdentity,
-    CandidateFact,
-    CanonicalFact,
-    Conflict,
-    ConflictState,
-    DecisionStatus,
-    EntityKind,
-    ProvenanceRecord,
-    RawEvidence,
-    Source,
-)
+from .domain import AutomotiveIdentity, CandidateFact, CanonicalFact, Conflict, ConflictState, DecisionStatus, EntityKind, ProvenanceRecord, RawEvidence, Source
 from .export import export_entity_json, export_entity_payload, write_entity_json
 from .fusion import FusionResult, fuse_candidates
 from .identity import MatchOutcome, ResolutionDecision, blocking_key, generate_candidates, resolve_pair
@@ -22,13 +12,15 @@ from .review import ReviewAction, ReviewDecision, review_conflict, review_eviden
 from .web_extraction import ExtractedWebFact, WebFieldRule, extract_autoevolution_artega_gt, extract_with_rules
 
 __all__ = [
-    "AutomotiveIdentity", "CandidateFact", "CanonicalFact", "Conflict", "ConflictState",
-    "DecisionStatus", "DocumentExtractionReport", "EntityKind", "EvidenceStore",
-    "ExtractedWebFact", "FusionResult", "IngestionReport", "MatchOutcome",
-    "NormalizationResult", "ProvenanceRecord", "RawEvidence", "ResolutionDecision",
-    "ReviewAction", "ReviewDecision", "Source", "ValidatedExtractionArtifact", "WebFieldRule",
-    "blocking_key", "export_entity_json", "export_entity_payload", "extract_autoevolution_artega_gt",
+    "AcquisitionCache", "AutomotiveIdentity", "CandidateFact", "CanonicalFact", "Checkpoint",
+    "Conflict", "ConflictState", "DecisionStatus", "DocumentExtractionReport", "EnrichmentJob",
+    "EntityKind", "EvidenceStore", "ExtractedWebFact", "FusionResult", "IngestionReport",
+    "JobResult", "JobState", "MatchOutcome", "NormalizationResult", "ProvenanceRecord",
+    "RateLimiter", "RawEvidence", "ResolutionDecision", "RetryPolicy", "ReviewAction",
+    "ReviewDecision", "Source", "ValidatedExtractionArtifact", "WebFieldRule", "blocking_key",
+    "export_entity_json", "export_entity_payload", "extract_autoevolution_artega_gt",
     "extract_ford_dark_horse_document", "extract_with_rules", "fuse_candidates", "generate_candidates",
-    "ingest_vehicle_makes_models_json", "normalize_fact", "resolve_pair", "review_conflict",
-    "review_evidence", "review_identity", "validate_extraction_artifact", "write_entity_json",
+    "identify_gaps", "ingest_vehicle_makes_models_json", "normalize_fact", "plan_jobs",
+    "resolve_pair", "review_conflict", "review_evidence", "review_identity", "run_job",
+    "validate_extraction_artifact", "write_entity_json",
 ]
