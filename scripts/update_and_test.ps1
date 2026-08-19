b0a5a10e0698d739f1835929148feb0dd3571d48
tihotm <igorbrito.n1@hotmail.com>
2026-08-19 20:02:50 -0300

    Add Windows update and sequential test runner

---
+++ scripts/update_and_test.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = "Stop"
+
+$RepoPath = "C:\Projetos\p7"
+$RepoUrl = "https://github.com/tihotm/podium7.git"
+
+if (-not (Test-Path $RepoPath)) {
+    git clone $RepoUrl $RepoPath
+}
+
+Set-Location $RepoPath
+
+git pull --ff-only
+python scripts\run_tests_one_by_one.py
