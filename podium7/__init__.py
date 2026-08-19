f2e63c61bdb6a6c46f32b6b6de322766834c8ebc
tihotm <igorbrito.n1@hotmail.com>
2026-08-19 18:35:17 -0300

    Expose entity resolution API

--- podium7/__init__.py
+++ podium7/__init__.py
@@ -10,6 +10,7 @@ from .domain import (
     RawEvidence,
     Source,
 )
+from .identity import MatchOutcome, ResolutionDecision, blocking_key, generate_candidates, resolve_pair
 from .ingestion import IngestionReport, ingest_vehicle_makes_models_json
 from .normalization import NormalizationResult, normalize_fact
 from .persistence import EvidenceStore
@@ -24,10 +25,15 @@ __all__ = [
     "EntityKind",
     "EvidenceStore",
     "IngestionReport",
+    "MatchOutcome",
     "NormalizationResult",
     "ProvenanceRecord",
     "RawEvidence",
+    "ResolutionDecision",
     "Source",
+    "blocking_key",
+    "generate_candidates",
     "ingest_vehicle_makes_models_json",
     "normalize_fact",
+    "resolve_pair",
 ]
