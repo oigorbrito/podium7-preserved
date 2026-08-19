03731791eb56f3a21e65841a7ba0ab882804dd75
tihotm <igorbrito.n1@hotmail.com>
2026-08-19 18:17:56 -0300

    Expose domain model

---
+++ podium7/__init__.py
@@ -0,0 +1,25 @@
+from .domain import (
+    AutomotiveIdentity,
+    CandidateFact,
+    CanonicalFact,
+    Conflict,
+    ConflictState,
+    DecisionStatus,
+    EntityKind,
+    ProvenanceRecord,
+    RawEvidence,
+    Source,
+)
+
+__all__ = [
+    "AutomotiveIdentity",
+    "CandidateFact",
+    "CanonicalFact",
+    "Conflict",
+    "ConflictState",
+    "DecisionStatus",
+    "EntityKind",
+    "ProvenanceRecord",
+    "RawEvidence",
+    "Source",
+]
