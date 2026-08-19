912512f7d1d39ed881ce55a77f60ade4c65f9b79
tihotm <igorbrito.n1@hotmail.com>
2026-08-19 18:37:56 -0300

    Expose export API

--- podium7/__init__.py
+++ podium7/__init__.py
@@ -10,6 +10,7 @@ from .domain import (
     RawEvidence,
     Source,
 )
+from .export import export_entity_json, export_entity_payload, write_entity_json
 from .fusion import FusionResult, fuse_candidates
 from .identity import MatchOutcome, ResolutionDecision, blocking_key, generate_candidates, resolve_pair
 from .ingestion import IngestionReport, ingest_vehicle_makes_models_json
@@ -34,9 +35,12 @@ __all__ = [
     "ResolutionDecision",
     "Source",
     "blocking_key",
+    "export_entity_json",
+    "export_entity_payload",
     "fuse_candidates",
     "generate_candidates",
     "ingest_vehicle_makes_models_json",
     "normalize_fact",
     "resolve_pair",
+    "write_entity_json",
 ]
