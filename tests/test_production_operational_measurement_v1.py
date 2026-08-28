61c7744ec28f240493dea33e0481bb048e07fc0c
Igor Brito <gestorigorbrito@gmail.com>
2026-08-28 05:25:27 -0300

    Update V1 operational measurement assertions for #167

--- tests/test_production_operational_measurement_v1.py
+++ tests/test_production_operational_measurement_v1.py
@@ -17,21 +17,21 @@ class ProductionOperationalMeasurementV1Tests(unittest.TestCase):
         measurement = measure_source_backed_operational_corpus(DATASETS)
         summary = measurement["summary"]
 
-        self.assertEqual(summary["total"], 60)
-        self.assertEqual(summary["created"], 21)
-        self.assertEqual(summary["matched"], 22)
-        self.assertEqual(summary["review"], 17)
+        self.assertEqual(summary["total"], 12)
+        self.assertEqual(summary["created"], 4)
+        self.assertEqual(summary["matched"], 4)
+        self.assertEqual(summary["review"], 4)
         self.assertEqual(summary["failed"], 0)
-        self.assertEqual(summary["catalogItems"], 21)
-        self.assertEqual(summary["openReviewTasks"], 17)
-        self.assertAlmostEqual(summary["automaticRate"], 43 / 60)
-        self.assertAlmostEqual(summary["reviewRate"], 17 / 60)
-        self.assertEqual(sum(measurement["actionsBySide"]["left"].values()), 30)
-        self.assertEqual(sum(measurement["actionsBySide"]["right"].values()), 30)
-        self.assertEqual(sum(measurement["reviewCauses"].values()), 17)
+        self.assertEqual(summary["catalogItems"], 4)
+        self.assertEqual(summary["openReviewTasks"], 4)
+        self.assertAlmostEqual(summary["automaticRate"], 8 / 12)
+        self.assertAlmostEqual(summary["reviewRate"], 4 / 12)
+        self.assertEqual(sum(measurement["actionsBySide"]["left"].values()), 6)
+        self.assertEqual(sum(measurement["actionsBySide"]["right"].values()), 6)
+        self.assertEqual(sum(measurement["reviewCauses"].values()), 4)
         self.assertEqual(measurement["reviewCauses"], {
-            "LABEL_AMBIGUITY": 3,
-            "MISSING_IDENTITY_EVIDENCE": 14,
+            "LABEL_AMBIGUITY": 1,
+            "MISSING_IDENTITY_EVIDENCE": 3,
         })
         self.assertNotIn("UNKNOWN_REVIEW_CAUSE", measurement["reviewCauses"])
 
