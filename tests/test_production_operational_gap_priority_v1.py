72821adc714287b1eda5824c89a84131ab7417e1
Igor Brito <gestorigorbrito@gmail.com>
2026-08-28 05:22:24 -0300

    Update V1 operational gap priority assertions for #167

--- tests/test_production_operational_gap_priority_v1.py
+++ tests/test_production_operational_gap_priority_v1.py
@@ -23,10 +23,10 @@ class ProductionOperationalGapPriorityV1Tests(unittest.TestCase):
             "MISSING_IDENTITY_EVIDENCE",
             "LABEL_AMBIGUITY",
         ])
-        self.assertEqual(priorities[0]["count"], 14)
+        self.assertEqual(priorities[0]["count"], 3)
         self.assertEqual(priorities[0]["disposition"], "ENRICH_IDENTITY_EVIDENCE")
-        self.assertAlmostEqual(priorities[0]["reviewShare"], 14 / 17)
-        self.assertEqual(priorities[1]["count"], 3)
+        self.assertAlmostEqual(priorities[0]["reviewShare"], 3 / 4)
+        self.assertEqual(priorities[1]["count"], 1)
         self.assertEqual(priorities[1]["disposition"], "ENRICH_LABEL_EVIDENCE")
         self.assertTrue(all("RESOLVER" not in item["disposition"] for item in priorities))
 
